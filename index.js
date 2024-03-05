e28a5889d6a22ef3fea73bff9ba2ef633308deb6
bobbyhadz <opmftw@gmail.com>
2024-03-05 17:52:25 +0200

    initial commit

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ console.log(getLastWeeksDate());
 
 // 👇️ Wed Jul 26 2023 10:46:35
 console.log(new Date());
+
+// -------------------------------------------------------------
+
+// // Get last week's Date preserving the time
+
+// function getLastWeeksDate() {
+//   const now = new Date();
+
+//   return new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+// }
+
+// // 👇️ Wed Jul 19 2023 10:47:53
+// console.log(getLastWeeksDate());
+
+// // 👇️ Wed Jul 26 2023 10:47:53
+// console.log(new Date());
